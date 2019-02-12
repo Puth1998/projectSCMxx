e067faeefc9c091cd6b39e6b84becfba696177d3
puth1998 <teenaputh12@hotmail.com>
2019-02-12 21:32:27 +0700

    Create emp sales

--- src/projectSCMxx/employees.java
+++ src/projectSCMxx/employees.java
@@ -1,5 +1,55 @@
 package projectSCMxx;
 
 public class employees {
+	private sales sale;
+	public String empNumber;
+	public String empName;
+	public int salary;
+
+	
+	public employees() {
+		
+	}
+	
+	public employees(String nb, String n, int s) {
+		this.empNumber = nb;
+		this.empName = n;
+		this.salary = s;
+	}
+
+
+	public String getEmpNumber() {
+		return empNumber;
+	}
+
+	public void setEmpNumber(String empNumber) {
+		this.empNumber = empNumber;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String emptName) {
+		this.empName = emptName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public sales getSale() {
+		return sale;
+	}
+
+	public void setSale(sales sale) {
+		this.sale = sale;
+	}
 
 }
--- src/projectSCMxx/sales.java
+++ src/projectSCMxx/sales.java
@@ -1,5 +1,31 @@
 package projectSCMxx;
 
 public class sales {
+	public String empNumber;
+	public int sales;
+
+	public sales() {
+	}
+	
+	public sales(String nb, int sales) {
+		this.empNumber = nb;
+		this.sales = sales;
+	}
+
+	public String getEmployeeNumber() {
+		return empNumber;
+	}
+
+	public void setEmployeeNumber(String empNumber) {
+		this.empNumber = empNumber;
+	}
+
+	public int getSales() {
+		return sales;
+	}
+
+	public void setSales(int sales) {
+		this.sales = sales;
+	}
 
 }
