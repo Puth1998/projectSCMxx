f8d55e3c5bd7c68c017cdad2a0543a0df820e04d
Fahg2541 <Fahg2541@gmail.com>
2019-02-12 21:49:22 +0700

    calSalary

--- src/projectSCMxx/calSalary.java
+++ src/projectSCMxx/calSalary.java
@@ -1,5 +1,38 @@
 package projectSCMxx;
 
 public class calSalary {
+	public int salary;
+	public int sales;
+
+	public calSalary() {
+		
+	}
+	
+	public calSalary(int s,int sl) {
+		this.salary = s;
+		this.sales = sl;
+		
+	}
+	
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+
+	public double sumsalary() {
+		double sumsalary = 0;
+		if( this.sales >= 1 && this.sales <= 50000){
+			sumsalary = (int) (this.sales*0.05);
+		}else {
+			sumsalary = (int) (this.sales*0.10);
+		}
+
+		return sumsalary+this.salary;
+	}
 
 }
