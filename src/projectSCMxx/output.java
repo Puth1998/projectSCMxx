7fbb5d60f6ef51b9c344c4dec05123b0552396d6
Namfon <mdjdjm317@gmail.com>
2019-02-12 21:38:48 +0700

    make output

--- src/projectSCMxx/output.java
+++ src/projectSCMxx/output.java
@@ -1,5 +1,7 @@
 package projectSCMxx;
 
 public class output {
-
+	public void print(String name, double sumsalary) {
+		System.out.print("Employee name "+ name + " Sum Salary = "+ sumsalary);			
+	}
 }
